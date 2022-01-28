6e8d13467ee086e383965ffda7f5004367cbce9c
dongnycklee <89719426+dongnycklee@users.noreply.github.com>
2022-01-28 10:42:08 +0900

    마이닝 블록 추가

--- src/main.js
+++ src/main.js
@@ -6,17 +6,29 @@ class Block{
     this.timestamp = timestamp;
     this.data = data;
     this.previousHash = previousHash;
-    this.hash = this.calculateHash();	
+    this.hash = this.calculateHash();
+    this.nonce = 0;
   }
 
   calculateHash(){
-    return SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data)).toString();
+    return SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data) + this.nonce).toString();
+  }
+
+  mineBlock(difficulty){
+    while(this.hash.substring(0, difficulty) !== Array(difficulty + 1).join("0")){
+      this.nonce++;
+      this.hash = this.calculateHash();
+  }
+
+    console.log("블록채굴:" +this.hash);
   }
 }
+
 
 class Blockchain{
   constructor(){
     this.chain = [this.createGenesisBlock()];
+    this.difficulty = 4;
   }
 
   createGenesisBlock(){
@@ -29,7 +41,7 @@ class Blockchain{
 
   addBlock(newBlock){
     newBlock.previousHash = this.getLatesBlock().hash;
-    newBlock.hash = newBlock.calculateHash();
+    newBlock.mineBlock(this.difficulty);
     this.chain.push(newBlock);
   }
 
@@ -52,13 +64,18 @@ class Blockchain{
 }
 
 let dongliCoin = new Blockchain();
-dongliCoin.addBlock(new Block(1, "28/01/2022", {amount: 4}));
-dongliCoin.addBlock(new Block(2, "28/01/2022", {amount: 10}));
+
+
+// console.log('채굴중 1...');
+// dongliCoin.addBlock(new Block(1, "28/01/2022", {amount: 4}));
+
+// console.log('채굴중 2...');
+// dongliCoin.addBlock(new Block(2, "28/01/2022", {amount: 10}));
 
 
 // 블록체인 인덱스의 1번을 직접 변조해보기
 // dongliCoin.chain[1].data = { amount: 100000 };
 
 
-console.log('블록들의 조작을 확인' + dongliCoin.isChainValid());
+// console.log('블록들의 조작을 확인' + dongliCoin.isChainValid());
 // console.log(JSON.stringify(dongliCoin, null, 4));
